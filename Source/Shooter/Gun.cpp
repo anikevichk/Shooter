bbcdbfc19dda6f4311b317481f953de7e244f1d9
Kate <01194270@pw.edu.pl>
2025-07-22 21:25:33 +0300

    added debug for player view point for correct shooting

--- Source/Shooter/Gun.cpp
+++ Source/Shooter/Gun.cpp
@@ -4,6 +4,7 @@
 #include "Gun.h"
 #include "Components\SkeletalMeshComponent.h"
 #include "Kismet\GameplayStatics.h"
+#include "DrawDebugHelpers.h"
 
 // Sets default values
 AGun::AGun()
@@ -36,6 +37,18 @@ void AGun::Tick(float DeltaTime)
 void AGun::PullTrigger()
 {
 	UGameplayStatics::SpawnEmitterAttached(MuzzleFlash, Mesh, TEXT("MuzzleFlashSocket"));
+
+	APawn* OwnerPawn = Cast<APawn>(GetOwner());
+	if(OwnerPawn == nullptr) return;
+
+	AController* OwnerController = OwnerPawn->GetController();
+	if(OwnerController == nullptr) return;
+
+	FVector Location;
+	FRotator Rotation;
+	OwnerController->GetPlayerViewPoint(Location, Rotation);
+
+	DrawDebugCamera(GetWorld(), Location, Rotation, 90.f, 1.f, FColor::Red, true);
 
 }
 
